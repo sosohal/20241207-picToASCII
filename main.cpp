f55221335131c0e2164ee9a2ceab69591ab42915
HS <hs@HSs-MacBook-Pro.local>
2024-12-08 10:34:16 -0800

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "Starting pic to ASCII art process... " << endl;
+    const char *outputPath = "asciiArt.txt";
+
+    cout << "ASCII art complete." << endl;
+    cout << "ASCII art saved to " << outputPath << endl;
+}
